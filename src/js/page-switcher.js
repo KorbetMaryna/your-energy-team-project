f915f7868bba729b56dad4f0c593e4be2c49b3da
Maryna-Korbet <korbet.maryna.developer@gmail.com>
2023-12-12 11:53:00 +0200

    Added header and mobile-menu

--- src/js/page-switcher.js
+++ src/js/page-switcher.js
@@ -1,1 +1,15 @@
-console.log(`test`)
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop();
+    
+    const navLinks = document.querySelectorAll(".header-nav-list-item a");
+
+    navLinks.forEach(function (link) {
+        const linkPage = link.getAttribute("href").split("/").pop();
+
+        if (currentPage === linkPage || (currentPage === "" && linkPage === "index.html")) {
+        link.classList.add("header-nav-current");
+        } else {
+        link.classList.remove("header-nav-current");
+        }
+    });
+});
