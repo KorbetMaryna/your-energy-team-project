b63463e9ec28fa0037681bfaf08c90dcc1ca1820
Maryna-Korbet <korbet.maryna.developer@gmail.com>
2023-12-14 11:13:50 +0200

    fix: bugs container and rename files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './js/page-switcher';
-import './js/mobile-menu';
+import './js/pageSwitcher';
+import './js/mobileMenu';
 import './js/modal';
-import './js/modal-rating';
+import './js/modalRating';
 import './js/exercises';
 import './js/quote';
