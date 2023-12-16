bfd4a2e55771f6205760b83bf84010a5248a016c
Maryna-Korbet <korbet.maryna.developer@gmail.com>
2023-12-16 22:59:37 +0200

    deleting copies of files

--- src/js/search/index.js
+++
@@ -1,2 +0,0 @@
-import './searchInput';
-import './searchApi';
--- src/main.js
+++ src/main.js
@@ -10,5 +10,4 @@ import './js/loader';
 import './js/search/search-input';
 import './js/handlerScrollUp';
 import './js/showPageError';
-// import './js/search/index';
 import './js/privacy&terms';
