88eabc4abd6862e0d60dd9931d39763898e82b29
Maryna-Korbet <korbet.maryna.developer@gmail.com>
2023-12-17 08:45:32 +0200

    changed styles loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,7 +1,16 @@
 export function toggleLoader(showLoader) {
+    const loaderContainer = document.querySelector('.loader-backdrop');
     const loader = document.querySelector('.loader-container');
+
     if (showLoader) {
+        loaderContainer.style.display = "block";
         loader.classList.remove('hidden');
-    }
-    loader.classList.add('hidden')
+        document.body.style.overflow = "hidden";
+    } 
+        loader.classList.add('hidden');
+        loaderContainer.style.display = "none";
+        document.body.style.overflow = ""; 
 }
+
+
+
