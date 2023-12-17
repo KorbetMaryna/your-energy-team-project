d678e24fd32f63b322e69489f9acc28f6a3ef8bf
KorbetMaryna <139797358+KorbetMaryna@users.noreply.github.com>
2023-12-17 20:13:06 +0000

    Deploying to gh-pages from @ KorbetMaryna/your-energy-team-project@fbcd0f1b5d32475e8e00e7ed2b448d9357886130 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{s as d}from"./assets/showPageError-d1fbf77f.js";import"./assets/vendor-1133ef0b.js";function f(t,e){return h(t===1).concat(w(t,e),k(t===e))}function w(t,e){let a="";for(let n=1;n<=e;n+=1){if(n<t-2){a=a.concat('<li><span class="pagination-number">...</span></li>'),n=t-3;continue}if(n>t+2){a=a.concat('<li><span class="pagination-number">...</span></li>');break}a=a.concat(`<li>
+import{s as d}from"./assets/showPageError-9ed80bf2.js";import"./assets/vendor-1133ef0b.js";function f(t,e){return h(t===1).concat(w(t,e),k(t===e))}function w(t,e){let a="";for(let n=1;n<=e;n+=1){if(n<t-2){a=a.concat('<li><span class="pagination-number">...</span></li>'),n=t-3;continue}if(n>t+2){a=a.concat('<li><span class="pagination-number">...</span></li>');break}a=a.concat(`<li>
         <button class="js-page pagination-number${n===t?" active":""}" type="button"  data-page-navigate=${n}>${n}</button>
       </li>`)}return`<ul class="fav-workouts-pagination-numbers">
       ${a}
